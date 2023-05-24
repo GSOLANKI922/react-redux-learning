37e6a5a64c6c62b526c9eb78b0efbf4213dc84d5
gautam_solanki <gautam.solanki@logicwind.com>
2023-05-24 18:07:49 +0530

    delete tmdb_next src

--- tmdb_next/src/pages/api/getAuthToken.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  const token = localStorage?.getItem("token");
-
-  res.status(200).json({ token });
-}
